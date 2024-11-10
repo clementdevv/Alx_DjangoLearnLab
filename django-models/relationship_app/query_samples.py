ea05b85329093f142ace8cd8e6e5fb782c63346e
clementdevv <clementdevv@gmail.com>
2024-11-10 23:36:30 +0300

    relocated-rel-app

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def get_books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        books = author.books.all()
+        books = Book.objects.filter(author=author)
         return books
     except Author.DoesNotExist:
         return []
